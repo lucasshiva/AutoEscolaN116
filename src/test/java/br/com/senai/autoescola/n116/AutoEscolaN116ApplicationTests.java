6952dab089e17ae712ace1cd4f5795c936ae3ebf
Lucas Silva <silva.lucasdev@gmail.com>
2026-05-04 15:14:20 -0300

    fix: use test profile for context tests

--- src/test/java/br/com/senai/autoescola/n116/AutoEscolaN116ApplicationTests.java
+++ src/test/java/br/com/senai/autoescola/n116/AutoEscolaN116ApplicationTests.java
@@ -1,14 +1,17 @@
 package br.com.senai.autoescola.n116;
 
+import br.com.senai.autoescola.n116.auth.AuthController;
 import br.com.senai.autoescola.n116.instructors.InstructorsController;
 import br.com.senai.autoescola.n116.students.StudentsController;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class AutoEscolaN116ApplicationTests {
 
     @Autowired
@@ -17,9 +20,13 @@ class AutoEscolaN116ApplicationTests {
     @Autowired
     private InstructorsController instructorsController;
 
+    @Autowired
+    private AuthController authController;
+
     @Test
     void contextLoads() {
         assertThat(studentsController).isNotNull();
         assertThat(instructorsController).isNotNull();
+        assertThat(authController).isNotNull();
     }
 }
